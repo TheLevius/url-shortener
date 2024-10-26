d33c9a9de2ec52a26625e6e4b6a69f831ed1cb78
TheLevius <reflevius@gmail.com>
2024-10-26 12:34:57 +0300

    feat: add logger and connect to sqlite db

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,14 +2,57 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener/cmd/internal/config"
+	"url-shortener/cmd/internal/lib/logger/sl"
+	"url-shortener/cmd/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
-	// TODO: init logger: slog
-	// TODO: init storage: sqLite
+	log := setupLogger(cfg.Env)
+	// log = log.With(slog.String("env", cfg.Env))
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	// id, err := storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	// 	log.Error("failed to save url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
+	// log.Info("saved url", slog.Int64("id: ", id))
+	// id, err = storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	// 	log.Error("failed to save url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
+	_ = storage
 	// TODO: init router: chi
-	// TODO: run server: 
+	// TODO: run server:
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
